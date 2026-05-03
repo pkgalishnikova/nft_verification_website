3b07d9255c3a69e24cbd405fb04a379ca16f8783
pkgalishnikova <120177604+pkgalishnikova@users.noreply.github.com>
2026-05-03 23:38:45 +0300

    Delete src/contract.ts

--- src/contract.ts
+++
@@ -1,7 +0,0 @@
-import { createThirdwebClient } from "thirdweb";
-
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || "placeholder";
-
-export const client = createThirdwebClient({
-  clientId: clientId,
-});
